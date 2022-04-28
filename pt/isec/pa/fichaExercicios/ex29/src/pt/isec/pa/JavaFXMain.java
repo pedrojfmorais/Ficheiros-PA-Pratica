9b1e576b7a964e3b53513f0c449782f8de1fe8a2
pedrojfmorais <pedrojfmorais@hotmail.com>
2022-04-28 18:43:45 +0100

    ex29

---
+++ pt/isec/pa/fichaExercicios/ex29/src/pt/isec/pa/JavaFXMain.java
@@ -0,0 +1,11 @@
+package pt.isec.pa;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXMain extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
---
+++ pt/isec/pa/fichaExercicios/ex29/src/pt/isec/pa/Main.java
@@ -0,0 +1,10 @@
+package pt.isec.pa;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main{
+    public static void main(String[] args) {
+        Application.launch(JavaFXMain.class, args);
+    }
+}
